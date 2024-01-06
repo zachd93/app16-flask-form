e02ccb3ded21ac795cdee40fc0e4df479a502700
zachd93 <zachdebolt1993@gmail.com>
2024-01-05 16:04:25 -0800

    Add post

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, requests
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if requests.method == "POST":
     return render_template("index.html")
 
 
